fb872db68b3aa073b07363c047ab7a5cfb075e53
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-24 17:07:44 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -116,7 +116,7 @@ Please enter a number between 1-10 on,a Song select!`)
                     
                     try{
                        var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
-                           maxMatches: 1,
+                           maxMatches: 10,
                            time: 10000,
                            errors: ['time']
                        }); 
